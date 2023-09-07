2be83828e896a28fc41f0fc103c12f8217a66ebb
bryankenote <bryan.kenote@faithlife.com>
2023-09-07 14:35:54 -0700

    add logging

--- src/Facility.LanguageServer/Program.cs
+++ src/Facility.LanguageServer/Program.cs
@@ -1,18 +1,34 @@
+using System.Globalization;
 using Facility.LanguageServer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OmniSharp.Extensions.LanguageServer.Server;
+using Serilog;
 
 ////while (!System.Diagnostics.Debugger.IsAttached)
 ////{
 ////	await Task.Delay(100);
 ////}
 
+Log.Logger = new LoggerConfiguration()
+	.Enrich.FromLogContext()
+	.WriteTo.File(path: "log.txt", rollingInterval: RollingInterval.Day, formatProvider: new DateTimeFormatInfo())
+	.MinimumLevel.Verbose()
+	.CreateLogger();
+
 var server = await LanguageServer.From(
 	options =>
 		options
 			.WithInput(Console.OpenStandardInput())
 			.WithOutput(Console.OpenStandardOutput())
+			.ConfigureLogging(
+				x => x
+					.AddSerilog(Log.Logger)
+					.AddLanguageProtocolLogging()
+					.SetMinimumLevel(LogLevel.Debug))
 			.WithHandler<FsdSyncHandler>()
 			.WithHandler<FsdDefinitionHandler>()
-			.WithHandler<FsdHoverHandler>()).ConfigureAwait(false);
+			.WithHandler<FsdHoverHandler>()
+			.WithServices(x => x.AddLogging(b => b.SetMinimumLevel(LogLevel.Trace)))).ConfigureAwait(false);
 
 await server.WaitForExit.ConfigureAwait(false);
